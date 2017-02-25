9f45dc79a33ea3644d959706338bf83709e9ca4d
cosinus <cosinus@cosinuskrakow>
2017-02-25 10:16:16 +0100

    first 1

---
+++ cw1/cw1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ćwiczenie 1</title>
+    </head>
+    <body>
+        <?php
+        $a = 12;
+        $b = 34;
+        echo "a = ".$a.'<br>';
+        echo "b = ".$b.'<br>';
+        ?>
+    </body>
+</html>
