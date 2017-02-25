4ad766ec9759a93251b6b37d303ca9ca1c3bd032
cosinus <cosinus@cosinuskrakow>
2017-02-25 13:54:15 +0100

    first 1

--- cw3/cw3.php
+++ cw3/cw3.php
@@ -16,11 +16,29 @@
             return $html;
         }
         function Lista($ile){
-            
+            $html = '<ul>';
+            for($i=1;$i<=$ile;$i++){
+                $html .= "<li>Element listy nr {$i}</li>\n";
+            }
+            return $html."</ul>\n";
+        }
+        function TabMnozenia($row, $col){
+            $html  = '<table>'."\n";
+            for($i=1;$i<=$row;$i++){                
+                $html .= '<tr>'."\n";
+                for($j=1;$j<=$col;$j++){
+                    $pierwszy = ($i==1 || $j==1)
+                            ?' class="first"': '';
+                    $html .= "\t".'<td'.$pierwszy.'>'.($i*$j).'</td>'."\n";
+                }
+                $html .= '</tr>'."\n";
+            }         
+            return $html.'</table>'."\n";
         }
         echo Wizytowka("Adam", "Nowak", "administrator");
         echo Wizytowka("Anna", "Bobek", "kierownik");
         echo Lista(5);
+        echo TabMnozenia(17, 8);
         ?>
     </body>
 </html>
