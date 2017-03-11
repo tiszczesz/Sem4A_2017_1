f924dc9a9b4c92f2d93f965bcc86b319c2e3aa54
cosinus <cosinus@cosinuskrakow>
2017-03-11 09:56:39 +0100

    first 1

--- cw9/wynik.php
+++ cw9/wynik.php
@@ -12,7 +12,10 @@
                 $a = floatval($_POST['a']);
                 $b = floatval($_POST['b']);
                 echo "<ul>\n";
-                echo "<li>{} + {} = ".($a+$b)."</p>"
+                echo "<li>{$a} + {$b} = ".($a+$b)."</li>";
+                 echo "<li>{$a} * {$b} = ".($a*$b)."</li>";
+                 $wynik = $b!=0 ? round($a/$b,2) : "Brak wyniku";
+                  echo "<li>{$a} / {$b} = ".$wynik."</li>";
             }else{
                 echo "<p>Błędne dane z formularza</p>\n";
             }
