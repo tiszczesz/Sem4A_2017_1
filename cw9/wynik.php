8c523d0edf9af7fa5c6593fb84d5f649d04a51ef
cosinus <cosinus@cosinuskrakow>
2017-03-11 09:57:31 +0100

    first 1

--- cw9/wynik.php
+++ cw9/wynik.php
@@ -16,6 +16,7 @@
                  echo "<li>{$a} * {$b} = ".($a*$b)."</li>";
                  $wynik = $b!=0 ? round($a/$b,2) : "Brak wyniku";
                   echo "<li>{$a} / {$b} = ".$wynik."</li>";
+                  echo "</ul>\n";
             }else{
                 echo "<p>Błędne dane z formularza</p>\n";
             }
