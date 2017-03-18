b9e2ed79d8e4e0e83ab9c8ab8255e61f5ab5dc3f
cosinus <cosinus@cosinuskrakow>
2017-03-18 10:24:22 +0100

    first 1

--- cw14/cw14.php
+++ cw14/cw14.php
@@ -4,12 +4,23 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="cw14.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php 
-        $items= scandir("gg");
-        var_dump($items);
+        <?php
+        $items = scandir("gg");
+        //var_dump($items);
+        foreach ($items as $value) {
+            if(is_dir("gg/".$value)){
+                 echo "<p>To jest katalog: <span class='wyr'>{$value}</span></p>\n";
+            }
+            if(is_file("gg/".$value)){
+                 echo "<p>To jest plik: <span class='wyr'>{$value}</span>";
+                 if(is_executable("gg/".$value)) echo " wykonywalny ";
+                 $size = filesize("gg/".$value);
+                 echo " rozmiar: {$size} </p>\n";
+            }           
+        }
         ?>
     </body>
 </html>
- 
